92e8b7826b9dbe44bf834807705d1b1d1fe8e00a
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-05-20 16:30:58 -0300

    Properties, Vinyl, ORM

--- catalogoteca/src/main/java/com/catalogoteca/catalogoteca/entities/Vinyl.java
+++ catalogoteca/src/main/java/com/catalogoteca/catalogoteca/entities/Vinyl.java
@@ -1,4 +1,112 @@
 package com.catalogoteca.catalogoteca.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_vinyl")
 public class Vinyl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String artist;
+    private String genre;
+    private String label;
+    private Integer releaseYear;
+    private String format;
+    private String imgUrl;
+
+    public Vinyl() {
+    }
+
+    public Vinyl(Long id, String title, String artist, String genre, String label, Integer releaseYear, String format, String imgUrl) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.label = label;
+        this.releaseYear = releaseYear;
+        this.format = format;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Vinyl other = (Vinyl) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
